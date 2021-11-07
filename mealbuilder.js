a581a1ccfb6d0d275b37fba8d89c61641538897f
Zach Reynolds <zacharyt.reynolds@gmail.com>
2021-11-06 20:24:17 -0400

    Bug fixes

--- mealbuilder.js
+++ mealbuilder.js
@@ -1,4 +1,4 @@
-const prices = {'Protein': 0, 'Chicken': 3, 'Pork': 2, 'Beef': 4, 'Vegetable': 0, 'Broccoli': 1, 'Lettuce': 0.50, 'Spinach': 1.25, 'Carbs': 0, 'Apple': 1.00, 'Corn': 1.00, 'Pasta': 2.00, 'Add-ons': 0, 'Ketchup': 0.50, 'BBQ Sauce': 0.50, 'Mayo': 0.50, 'Condiments': 0, 'Pudding': 1.50, 'Corn Bread': 1.25, 'Soup': 1.75};
+const prices = {'Protein': 0, 'Chicken': 3, 'Pork': 2, 'Beef': 4, 'Vegetables': 0, 'Broccoli': 1, 'Lettuce': 0.50, 'Spinach': 1.25, 'Carbs': 0, 'Apple': 1.00, 'Corn': 1.00, 'Pasta': 2.00, 'Add-ons': 0, 'Ketchup': 0.50, 'BBQ Sauce': 0.50, 'Mayo': 0.50, 'Condiments': 0, 'Pudding': 1.50, 'Corn Bread': 1.25, 'Soup': 1.75};
 
 document.getElementById('protein').addEventListener('click', () => {
     const prot = document.getElementById('protein').value;
@@ -69,5 +69,5 @@ document.getElementById('protein').addEventListener('click', () => {
     const carb = document.getElementById('carbs').value;
     const prot = document.getElementById('protein').value;
     const price = prices[ao] + prices[cond] + prices[veg] + prices[carb] + prices[prot];
-    document.getElementById('total').innerHTML = '$' + price;
+    document.getElementById('total').innerHTML = 'Total: $' + price;
 });
