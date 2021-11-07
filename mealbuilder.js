5c96182f6258a90475e65ac6d6263c4f10dac1f4
Rishab Chakravarthy <rchakravarth@umass.edu>
2021-11-06 20:34:36 -0400

    finished server.js

--- mealbuilder.js
+++ mealbuilder.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const macros = {'fats':0, 'carbs':0, 'protein': 0};
+
 const prices = {'Protein': 0, 'Chicken': 3, 'Pork': 2, 'Beef': 4, 'Vegetable': 0, 'Broccoli': 1, 'Lettuce': 0.50, 'Spinach': 1.25, 'Carbs': 0, 'Apple': 1.00, 'Corn': 1.00, 'Pasta': 2.00, 'Add-ons': 0, 'Ketchup': 0.50, 'BBQ Sauce': 0.50, 'Mayo': 0.50, 'Condiments': 0, 'Pudding': 1.50, 'Corn Bread': 1.25, 'Soup': 1.75};
 
 document.getElementById('protein').addEventListener('click', () => {
--- server.js
+++ server.js
@@ -5,28 +5,56 @@ import express from 'express';
 import http from 'http';
 import url from 'url';
 
-const server = http.createServer(async (req, res) => {
-    const parsed = url.parse(req.url, true);
+const 
 
-    if (parsed.pathname === '/user') {
-        let body = '';
-        req.on('data', data => body += data);
-        req.on('end', () => {
-            const data = JSON.parse(body);
-        });
-    } else if (parsed.pathname === '/user/new') {
-        let body = '';
-        req.on('data', data => body += data);
-        req.on('end', () => {
-            const data = JSON.parse(body);
-        });
-    } else if (parsed.pathname === '/user/id/saved-meals') {
-        res.end();
-    } else if (parsed.pathname === '/user/id/mealbuilder') {
-        res.end();
-    } else {
-        res.writeHead(404);
-        res.end();
-    }
-}).listen(8080);
+const app = express();
+const port = 3000;
+
+app.use(express.json()); // lets you handle JSON input
+app.use(express.static('public'));
+
+app.get('/user/id/saved-meals',(req, res) => {
+    res.send({"name: ": faker.name.findName()})
+});
+
+app.get('/user',(req, res) => {
+    res.send({"name: ": faker.name.findName()})
+});
+
+app.get('/user/new',(req, res) => {
+    res.send({"name: ": faker.name.findName()})
+});
+
+app.get('/user/id/mealbuilder',(req, res) => {
+    res.send({"name: ": faker.name.findName()})
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// const server = http.createServer(async (req, res) => {
+//     const parsed = url.parse(req.url, true);
+
+//     if (parsed.pathname === '/user') {
+//         let body = '';
+//         req.on('data', data => body += data);
+//         req.on('end', () => {
+//             const data = JSON.parse(body);
+//         });
+//     } else if (parsed.pathname === '/user/new') {
+//         let body = '';
+//         req.on('data', data => body += data);
+//         req.on('end', () => {
+//             const data = JSON.parse(body);
+//         });
+//     } else if (parsed.pathname === '/user/id/saved-meals') {
+//         res.end();
+//     } else if (parsed.pathname === '/user/id/mealbuilder') {
+//         res.end();
+//     } else {
+//         res.writeHead(404);
+//         res.end();
+//     }
+// }).listen(8080);
 
