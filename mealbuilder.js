b51d43a1ac54b61d01e79a53150317dbd190ab70
Rishab Chakravarthy <rchakravarth@umass.edu>
2021-11-24 22:43:11 -0500

    made changes to food_database.json, mealbuilder.js, meal_builder.html

--- mealbuilder.js
+++ mealbuilder.js
@@ -1,71 +1,98 @@
 'use strict';
 
+import fetch from "node-fetch";
+
 const macros = {'fats':0, 'carbs':0, 'protein': 0};
 
 const prices = {'Protein': 0, 'Chicken': 3, 'Pork': 2, 'Beef': 4, 'Vegetable': 0, 'Broccoli': 1, 'Lettuce': 0.50, 'Spinach': 1.25, 'Carbs': 0, 'Apple': 1.00, 'Corn': 1.00, 'Pasta': 2.00, 'Add-ons': 0, 'Ketchup': 0.50, 'BBQ Sauce': 0.50, 'Mayo': 0.50, 'Condiments': 0, 'Pudding': 1.50, 'Corn Bread': 1.25, 'Soup': 1.75};
 
-document.getElementById('protein').addEventListener('click', () => {
-    const prot = document.getElementById('protein').value;
-    const price = prices[prot];
-    document.getElementById('Prot').innerHTML = prot + ': $' + price;
-});
-document.getElementById('carbs').addEventListener('click', () => {
-    const carb = document.getElementById('carbs').value;
-    const price = prices[carb];
-    document.getElementById('Carb').innerHTML = carb + ': $' + price;
-});
-document.getElementById('vegetables').addEventListener('click', () => {
-    const veg = document.getElementById('vegetables').value;
-    const price = prices[veg];
-    document.getElementById('Veg').innerHTML = veg + ': $' + price;
-});
-document.getElementById('condiments').addEventListener('click', () => {
-    const cond = document.getElementById('condiments').value;
-    const price = prices[cond];
-    document.getElementById('Cond').innerHTML = cond + ': $' + price;
-});
-document.getElementById('addons').addEventListener('click', () => {
-    const ao = document.getElementById('addons').value;
-    const price = prices[ao];
-    document.getElementById('ao').innerHTML = ao + ': $' + price;
-});
+const url = "https://raw.githubusercontent.com/rchakravarth/326-final-teamphi/main/docs/food_database.json";
 
-document.getElementById('addons').addEventListener('click', () => {
-    const ao = document.getElementById('addons').value;
-    const veg = document.getElementById('vegetables').value;
-    const cond = document.getElementById('condiments').value;
-    const carb = document.getElementById('carbs').value;
-    const prot = document.getElementById('protein').value;
-    const price = prices[ao] + prices[cond] + prices[veg] + prices[carb] + prices[prot];
-    document.getElementById('total').innerHTML = '$' + price;
-});
-document.getElementById('condiments').addEventListener('click', () => {
-    const ao = document.getElementById('addons').value;
-    const veg = document.getElementById('vegetables').value;
-    const cond = document.getElementById('condiments').value;
-    const carb = document.getElementById('carbs').value;
-    const prot = document.getElementById('protein').value;
-    const price = prices[ao] + prices[cond] + prices[veg] + prices[carb] + prices[prot];
-    document.getElementById('total').innerHTML = '$' + price;
-});
-document.getElementById('vegetables').addEventListener('click', () => {
-    const ao = document.getElementById('addons').value;
-    const veg = document.getElementById('vegetables').value;
-    const cond = document.getElementById('condiments').value;
-    const carb = document.getElementById('carbs').value;
-    const prot = document.getElementById('protein').value;
-    const price = prices[ao] + prices[cond] + prices[veg] + prices[carb] + prices[prot];
-    document.getElementById('total').innerHTML = '$' + price;
+// Function that retrieves food data from static JSON file.
+async function getData(url) {
+    const response = await fetch(url);
+    let obj;
+    if(response.ok){
+        obj = await response.json();
+    }else{
+        console.log("error");
+    }
+    return obj;
+}
+
+// 
+const foodData = await getData(url);
+
+document.getElementById('protein').addEventListener('click', () => {
+    const protein = document.getElementById('protein').value;
+    const protein_price = foodData["protein"][protein].price;
+    document.getElementById('Prot').innerHTML = protein + ': $' + protein_price;
 });
 
 document.getElementById('carbs').addEventListener('click', () => {
+    const carb = document.getElementById('carbs').value;
+    const carb_price = foodData["carbs"][carb].price;
+    document.getElementById('Carb').innerHTML = carb + ': $' + carb_price;
+});
+
+document.getElementById('vegetables').addEventListener('click', () => {
+    const veg = document.getElementById('vegetables').value;
+    const veg_price = foodData["vegetables"][veg].price;
+    document.getElementById('Veg').innerHTML = veg + ': $' + veg_price;
+});
+
+document.getElementById('condiments').addEventListener('click', () => {
+    const cond = document.getElementById('condiments').value;
+    const cond_price = foodData["condiments"][cond].price;
+    document.getElementById('Cond').innerHTML = cond + ': $' + cond_price;
+});
+
+document.getElementById('addons').addEventListener('click', () => {
+    const addons = document.getElementById('addons').value;
+    const addons_price = foodData["condiments"][cond].price;
+    document.getElementById('ao').innerHTML = addons + ': $' + addons_price;
+});
+
+document.getElementById('addons').addEventListener('click', () => {
     const ao = document.getElementById('addons').value;
     const veg = document.getElementById('vegetables').value;
     const cond = document.getElementById('condiments').value;
     const carb = document.getElementById('carbs').value;
     const prot = document.getElementById('protein').value;
-    const price = prices[ao] + prices[cond] + prices[veg] + prices[carb] + prices[prot];
-    document.getElementById('total').innerHTML = '$' + price;
+    const total = foodData["protein"][prot].price + foodData["addons"][ao].price + foodData["vegetables"][veg].price + 
+    foodData["carbs"][carb].price + foodData["condiments"][cond].price;
+    document.getElementById('total').innerHTML = '$' + total;
+});
+
+document.getElementById('condiments').addEventListener('click', () => {
+    const ao = document.getElementById('addons').value;
+    const veg = document.getElementById('vegetables').value;
+    const cond = document.getElementById('condiments').value;
+    const carb = document.getElementById('carbs').value;
+    const prot = document.getElementById('protein').value;
+    const total = foodData["protein"][prot].price + foodData["addons"][ao].price + foodData["vegetables"][veg].price + 
+    foodData["carbs"][carb].price + foodData["condiments"][cond].price;
+    document.getElementById('total').innerHTML = '$' + total;
+});
+document.getElementById('vegetables').addEventListener('click', () => {
+    const ao = document.getElementById('addons').value;
+    const veg = document.getElementById('vegetables').value;
+    const cond = document.getElementById('condiments').value;
+    const carb = document.getElementById('carbs').value;
+    const total = foodData["protein"][prot].price + foodData["addons"][ao].price + foodData["vegetables"][veg].price + 
+    foodData["carbs"][carb].price + foodData["condiments"][cond].price;
+    document.getElementById('total').innerHTML = '$' + total;
+});
+
+document.getElementById('carbs').addEventListener('click', () => {
+    const ao = document.getElementById('addons').value;
+    const veg = document.getElementById('vegetables').value;
+    const cond = document.getElementById('condiments').value;
+    const carb = document.getElementById('carbs').value;
+    const prot = document.getElementById('protein').value;
+    const total = foodData["protein"][prot].price + foodData["addons"][ao].price + foodData["vegetables"][veg].price + 
+    foodData["carbs"][carb].price + foodData["condiments"][cond].price;
+    document.getElementById('total').innerHTML = '$' + total;
 });
 
 document.getElementById('protein').addEventListener('click', () => {
@@ -74,6 +101,11 @@ document.getElementById('protein').addEventListener('click', () => {
     const cond = document.getElementById('condiments').value;
     const carb = document.getElementById('carbs').value;
     const prot = document.getElementById('protein').value;
-    const price = prices[ao] + prices[cond] + prices[veg] + prices[carb] + prices[prot];
-    document.getElementById('total').innerHTML = 'Total: $' + price;
+    const total = foodData["protein"][prot].price + foodData["addons"][ao].price + foodData["vegetables"][veg].price + 
+    foodData["carbs"][carb].price + foodData["condiments"][cond].price;
+    document.getElementById('total').innerHTML = '$' + total;
 });
+
+// document.getElementById("save").addEventListener('click', () =>{
+    
+// })
